052ee5f132cf2b4ece2a46c8b7ee72cd1d61833a
Prathamesh Chavan <prathamesh.chavan216@gmail.com>
2023-11-30 13:50:22 +0530

    added authentication route

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,7 @@ import compression from "compression";
 import cors from "cors";
 import mongoose from "mongoose";
 import "dotenv/config";
+import router from "router";
 
 const app = express();
 
@@ -28,3 +29,5 @@ server.listen(8080, () => {
 mongoose.Promise = Promise;
 mongoose.connect(process.env.MONGO_URL);
 mongoose.connection.on("error", (error: Error) => console.log(error));
+
+app.use(router());
