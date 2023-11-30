1daa803b7d1ab4cf2adca5f89d9bf58fbba31c9f
Prathamesh Chavan <prathamesh.chavan216@gmail.com>
2023-11-30 15:59:59 +0530

    added delete user route

--- src/router/users.ts
+++ src/router/users.ts
@@ -1,9 +1,9 @@
 import { Router } from "express";
 
 import { deleteUser, getAllUsers } from "../controllers/users";
-import { isAuthenticated } from "../middlewares";
+import { isAuthenticated, isOwner } from "../middlewares";
 
 export default (router: Router) => {
 	router.get("/users", isAuthenticated, getAllUsers);
-	router.delete("/users/:id", deleteUser);
+	router.delete("/users/:id", isAuthenticated, isOwner, deleteUser);
 };
