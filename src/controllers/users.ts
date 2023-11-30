1e3cba44b8917fa04becd73c695ca434ccf12624
Prathamesh Chavan <prathamesh.chavan216@gmail.com>
2023-11-30 18:07:40 +0530

    added update user controller

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from "express";
-import { deleteUserById, getUsers } from "../db/users";
+import { deleteUserById, getUserById, getUsers } from "../db/users";
 
 export const getAllUsers = async (req: Request, res: Response) => {
 	try {
@@ -19,6 +19,25 @@ export const deleteUser = async (req: Request, res: Response) => {
 		const deletedUser = await deleteUserById(id);
 
 		return res.status(200).json(deletedUser);
+	} catch (error) {
+		console.log(error);
+		return res.status(500).send("Internal Server Error");
+	}
+};
+
+export const updateUser = async (req: Request, res: Response) => {
+	try {
+		const { id } = req.params;
+		const { username } = req.body;
+
+		if (!username) return res.status(400).send("Bad Request");
+
+		const user = await getUserById(id);
+
+		user.username = username;
+		await user.save();
+
+		return res.status(200).json(user);
 	} catch (error) {
 		console.log(error);
 		return res.status(500).send("Internal Server Error");
