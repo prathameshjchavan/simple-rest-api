94b1a4e363338c976ccf9e03c4eca319d2d6c59e
Prathamesh Chavan <prathamesh.chavan216@gmail.com>
2023-11-30 14:56:39 +0530

    added login controller

--- src/controllers/authentication.ts
+++ src/controllers/authentication.ts
@@ -2,6 +2,43 @@ import { createUser, getUserByEmail } from "../db/users";
 import { Request, Response } from "express";
 import { random, authentication } from "../helpers";
 
+export const login = async (req: Request, res: Response) => {
+	try {
+		const { email, password } = req.body;
+
+		if (!email || !password) return res.status(400).send("Bad Request");
+
+		const user = await getUserByEmail(email).select(
+			"+authentication.salt +authentication.password"
+		);
+
+		if (!user) return res.status(404).send("User Not found");
+
+		const expectedHash = authentication(user.authentication.salt, password);
+
+		if (user.authentication.password !== expectedHash)
+			return res.status(403).send("Wrong Password");
+
+		const salt = random();
+		user.authentication.sessionToken = authentication(
+			salt,
+			user._id.toString()
+		);
+
+		await user.save();
+
+		res.cookie("AUTH", user.authentication.sessionToken, {
+			domain: "localhost",
+			path: "/",
+		});
+
+		return res.status(200).json(user);
+	} catch (error) {
+		console.log(error);
+		return res.status(400).send("Internal Server Error");
+	}
+};
+
 export const register = async (req: Request, res: Response) => {
 	try {
 		const { email, password, username } = req.body;
