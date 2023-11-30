d9db67b2075c5f05b8efd3c7fa29bf1b5b69c5d2
Prathamesh Chavan <prathamesh.chavan216@gmail.com>
2023-11-30 15:41:03 +0530

    added getAllUsers route

---
+++ src/router/users.ts
@@ -0,0 +1,8 @@
+import { Router } from "express";
+
+import { getAllUsers } from "../controllers/users";
+import { isAuthenticated } from "../middlewares";
+
+export default (router: Router) => {
+	router.get("/users", isAuthenticated, getAllUsers);
+};
